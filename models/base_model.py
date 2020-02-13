6aed812b4e4262106317db4c676fa707a14729c6
ACholberton <1103@holbertonschool.com>
2020-02-13 19:41:33 +0000

    added methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-""" This is our base model for the AIRBNB clone """
+
+import uuid
+from datetime import date datetime time
 
 class BaseModel:
+    def __init__(self, id, save, to_dict, created_at, updated_at):
+        self.id = str(uuid.uuid4())
+        self.save = save
+        self.to_dict =
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
