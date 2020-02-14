11e5c6af66e6f0fd8bcc3ef4131b498acf48f015
Tr3v1n4t0r <813@holbertonschool.com>
2020-02-14 10:19:29 -0800

    base model made

--- models/base_model.py
+++ models/base_model.py
@@ -11,3 +11,18 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    def __str__(self):
+        """Prints the BaseModel attributes"""
+        return ('[' + str(type(self).__name__) + '] (' + str(self.id) + ') ' +
+                str(self.__dict__))
+
+    def save(self):
+        """Update the attribute 'updated_at' with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict.update({'__class__': str(type(self).__name__)})
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
