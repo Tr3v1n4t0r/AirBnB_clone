d11dd2c48bde4386e999644a42b440118f612234
ACholberton <1103@holbertonschool.com>
2020-02-15 18:19:02 +0000

    Created init with args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,25 @@ from datetime import date, datetime, time
 
 class BaseModel:
     """BaseModel Class"""
-    def __init__(self, id=None, save, to_dict, created_at, updated_at):
-        self.id = str(uuid.uuid4())
-        self.save = save
-        self.to_dict = to_dict
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) is not 0:
+            for key, value in kwargs.items():
+                if key in "id":
+                    self.id = value
+                elif key in "created_at":
+                    self.created_at = datetime.now()
+                elif key in "updated_at":
+                    self.updated_at = datetime.now()
+                elif key in "to_dict":
+                    self.to_dict = to_dict
+                elif key in "save":
+                    self.save = save
+        else:
+            self.id = str(uuid.uuid4())
+            self.save = save
+            self.to_dict = to_dict
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Prints the BaseModel attributes"""
