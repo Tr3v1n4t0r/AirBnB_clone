998d6d9b30e6fc1c3d806327975545a515356834
ACholberton <1103@holbertonschool.com>
2020-02-15 00:53:34 +0000

    Updated base _models file

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 import uuid
-from datetime import date datetime time
+from datetime import date, datetime, time
 
 class BaseModel:
-    def __init__(self, id, save, to_dict, created_at, updated_at):
+    """BaseModel Class"""
+    def __init__(self, id=None, save, to_dict, created_at, updated_at):
         self.id = str(uuid.uuid4())
         self.save = save
-        self.to_dict =
+        self.to_dict = to_dict
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
