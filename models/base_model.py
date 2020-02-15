7c7fd12bba87899f06eab042a02038d7eb4a5b87
Tr3v1n4t0r <813@holbertonschool.com>
2020-02-15 10:37:44 -0800

    removed unneeded lines

--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,6 @@ class BaseModel:
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.save = save
-            self.to_dict = to_dict
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
