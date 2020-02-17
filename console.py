b600bf5511b7dcc6c4e1e7f3b1957bd83f469ca0
ACholberton <1103@holbertonschool.com>
2020-02-17 18:32:44 +0000

    Included startline for create, show, all, update and destroy commands

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 
 import cmd
 import json
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Class inherits cmd module import"""
@@ -15,12 +16,48 @@ class HBNBCommand(cmd.Cmd):
     do_EOF = do_quit
     """Handles the EOF"""
 
+    def do_create(self, value):
+        """Creates a new instance of a class, prints its ID and saves it"""
+        args = value
+        if (len(args) == 0):
+            print("** class name missing **")
+            return
+
+    def do_show(self, value):
+        """Prints the string representation of an instance based on the class name and id"""
+        args = value
+        if (len(args) == 0):
+            print("** class name missing **")
+            return
+
+    def do_all(self, value):
+        """Prints all string representation of all instances based or not on the class name"""
+        args = value
+        if args is None:
+            print("** class doesn't exist **")
+            return
+
+    def do_destroy(self, value):
+        """Deletes an instance based on the class name and id"""
+        args = value
+        if (len(args) == 0):
+            print("** class name missing **")
+            return
+
+        if args is None:
+            print("** class doesn't exist **")
+            return
+
+    def do_update(self, value):
+        """Updates an instance based on the class name and id by adding or updating attribute"""
+        args = value
+        if (len(args) == 0):
+            print("** class name missing **")
+            return
+
+        if args is None:
+            print("** class doesn't exist **")
+            return
+            
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
-    """"def do_create(self):
-        if 
-    def do_show(self):
-    def do_destroy(self):
-    def do_all(self):
-    def do_update(self):"""
