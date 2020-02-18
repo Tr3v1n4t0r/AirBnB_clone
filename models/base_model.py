ef39b21b36c12e3b927765c5d07ad5f504786ae2
Tr3v1n4t0r <813@holbertonschool.com>
2020-02-18 12:01:30 -0800

    forgot to make it save

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Defines a BaseModel
 """
 import uuid
 from datetime import date, datetime, time
+import models
 
 
 class BaseModel:
@@ -22,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Prints the BaseModel attributes"""
@@ -31,6 +33,7 @@ class BaseModel:
     def save(self):
         """Update the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         new_dict = self.__dict__.copy()
