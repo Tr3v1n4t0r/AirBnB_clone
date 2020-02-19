3a59382c69866fe95ff4fd45630e6e0d7b0e9499
Tr3v1n4t0r <813@holbertonschool.com>
2020-02-19 10:58:35 -0800

    added some modules

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""Entry point of the command interpreter"""
+import traceback
+import sys
 import cmd
 import shlex
 from models import storage
@@ -10,6 +12,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
     """Class inherits cmd module import"""
